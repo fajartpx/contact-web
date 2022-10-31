1d1cd66e9f7ff9594c7f7fc475c782c4b94c6adf
fajartpx <fajartpx@gmail.com>
2022-10-31 21:54:27 +0700

    use State

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,27 +1,23 @@
-import React from "react";
+import { TextField } from "@mui/material";
+import React, { useState } from "react";
 import '../App.css'
 
 
 
 
-const TodoForm =()=> {
+const TodoForm =({submitHandler, todo, changeHandler})=> {
+    const [input,setInput] = useState("")
 
     return (
         <>   
             <div className="todo-Form_container">
                 <h2>Data Kontak</h2>
                 <div className="todo-Form_desain">
-                    <input />
+                    <TextField value={input} onChange={(event)=>(setInput(event.target.value))} className="todo-form" label="Nama Lengkap" variant="outlined" />
+                    <p>{input}</p>
                 </div>
-               <div className="todo-Form_desain">
-                    <input />
-                </div>
-                 <div className="todo-Form_desain">
-                    <input />
-                </div>
-                <button className="todo-Form_button">Inputkan</button>
+                <button onClick={()=>(submitHandler(todo))} className="todo-Form_button">Inputkan</button>
             </div>
-            
         </>
     )
 }
--- src/containers/Todo.jsx
+++ src/containers/Todo.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoForm from '../components/TodoForm'
 import TodoList from '../components/TodoList'
 import '../App.css'
 
 const Todo =()=> {
+    const data = ["fajar", "ika"];
 
+    const [todo, setTodo] = useState([...data])
+    
+    const handlerSubmit = (dataBaru)=>{
+        const x= todo.push(dataBaru);
+        setTodo(x)
+    }
 
+   
     return(
         <>
         <h1 style={{textAlign:'center'}}>Call a Friend</h1>
         <hr/>
         <div className="todo-Container">
-            <TodoForm/>
+            <TodoForm submitHandler={handlerSubmit}/>
             <TodoList/>   
         </div>
         </> )
